a83d8d4fa4912e3f1ca84263b975e82572d24cc1
AmineNeifer <amineneifer2000@gmail.com>
2019-09-23 19:41:54 +0000

    oml

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+return (0);
+}
