bbe870178621c23dbe562e642810884dc3f7f8c6
anaruzz <laabidigh@gmail.com>
2019-09-24 15:12:12 +0000

    lol

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ * @a: int type
+ * @b: int type
+ * Return: the result of the addition of two numbers.
+ */
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
