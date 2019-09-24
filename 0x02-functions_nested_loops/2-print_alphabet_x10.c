eb49eb14f8c2b076bf15e8405ae7fae324368dec
anaruzz <laabidigh@gmail.com>
2019-09-24 09:21:06 +0000

    test

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ * char x;
+ * int i;
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char x;
+for (i = 1; i <= 10; i++)
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
