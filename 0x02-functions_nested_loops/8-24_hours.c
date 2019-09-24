3e2c4ffe767552a255285117d6bae8608391a8b9
anaruzz <laabidigh@gmail.com>
2019-09-24 13:34:42 +0000

    nice

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * jack_bauer - prints time from 00::00 to 23:59.
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+int n, c;
+for (n = 0; n <= 23; n++)
+for (c = 0; c <= 59; c++)
+{
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+_putchar(':');
+_putchar((n / 10) + '0');
+_putchar((c % 10) + '0');
+_putchar('\n');
+}
+}
