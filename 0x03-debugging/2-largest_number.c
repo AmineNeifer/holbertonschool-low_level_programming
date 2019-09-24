5faa6c90ff88d12ca30327b815653d4a9ea2c5bf
anaruzz <laabidigh@gmail.com>
2019-09-24 18:31:57 +0000

    the buggy man

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,17 +9,19 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
-if ((a > b && b > c) || (a > c && c > b))
+if (a > b && a > c)
 {
 largest = a;
 }
-else if (b > a && a > c || ( b > c && c > a))
+else if (b > a && b > c)
 {
 largest = b;
 }
-else
+else if (a == b)
 {
-largest = c;
+largest = a;
 }
+else
+largest = c;
 return (largest);
 }
