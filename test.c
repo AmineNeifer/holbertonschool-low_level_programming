177e588476b718c28849234c1836810d9c9c8bf7
AmineNeifer <amineneifer2000@gmail.com>
2019-09-25 20:45:18 +0000

    litterally a test file

--- test.c
+++ test.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
 int main(void)
 {
-    int i, j;
-    for (i = 0; i <= 9; i++)
+  int i, j, n;
+  n = 12;
+    if (n < 15 && n > 0)
       {
-	for (j = 0; j <= 9; j++)
+    for (i = 0; i <= n; i++)
+      {
+	for (j = 0; j <= n; j++)
 	  {
-	   
-	    if (((i * j) / 10) != 0)
+	   if (j == 0)
+	      putchar ('0');
+	   else if (((i * j) / 100) != 0)
 	      {
-		putchar(((i * j) / 10) + '0');
+		putchar((i * j / 100) + '0');
+		putchar(((i * j / 10) % 10) + '0');
 		putchar(((i * j) % 10) + '0');
 	      }
+	   else if (((i * j) / 10) != 0)
+	     {
+	       putchar(' ');
+	       putchar(((i * j) / 10) + '0');
+	       putchar(((i *j) % 10) + '0');
+	     }
 	    else
 	      {
 		putchar(' ');
+		putchar(' ');
 		putchar((i * j) + '0');
 	      }
-	    if (j != 9)
+	    if (j != n)
 	      {
 		putchar(',');
 		putchar(' ');
 	      }
 	  }
 	putchar('\n');
+      }
       }
 return (0);
 }
