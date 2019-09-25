a467d148a9c7cf809819bb53cd28b627f2f1eb2c
AmineNeifer <amineneifer2000@gmail.com>
2019-09-25 20:52:43 +0000

    printing n times table

---
+++ 0x02-functions_nested_loops/100-timestable.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+
+/**
+ * times_table - pritns the 9 times table.
+ *
+ * Return: Always 0.
+ */
+void times_table(int n)
+{
+int i, j;
+if (n < 15 && n > 0)
+{
+for (i = 0; i <= n; i++)
+}
+for (j = 0; j <= n; j++)
+{
+if (j == 0)
+_putchar ('0');
+else if (((i * j) / 100) != 0)
+{
+_putchar((i * j / 100) + '0');
+_putchar(((i * j / 10) % 10) + '0');
+_putchar(((i * j) % 10) + '0');
+}
+else if (((i * j) / 10) != 0)
+{
+_putchar(' ');
+_putchar(((i * j) / 10) + '0');
+_putchar(((i *j) % 10) + '0');
+}
+else
+{
+_putchar(' ');
+_putchar(' ');
+_putchar((i * j) + '0');
+}
+if (j != n)
+}
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
+}
