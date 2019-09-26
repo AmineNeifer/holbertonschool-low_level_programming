13baec103aee098f9e6c0ba11fb9e2ba179c4660
anaruzz <laabidigh@gmail.com>
2019-09-26 12:44:28 +0000

    squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_square - draws a square.
+ * @size: int type.
+ * Return: none.
+ */
+void print_square(int size)
+{
+  int i, j;
+  if (size > 0)
+    for (j = 1; j <= size; j++)
+      {
+    for (i = 1; i <= size; i++)
+      _putchar('#');
+  _putchar('\n');
+      }
+  else 
+    _putchar('\n');
+}
