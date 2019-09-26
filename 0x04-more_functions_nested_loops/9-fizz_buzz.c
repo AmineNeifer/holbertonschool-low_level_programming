86e3c52b536e7ffdc30d6ee813c8328ee1a66fe8
anaruzz <laabidigh@gmail.com>
2019-09-26 14:02:49 +0000

    fizz-buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,9 +13,14 @@ printf("FizzBuzz ");
 else if ((i % 3) == 0)
 printf("Fizz ");
 else if ((i % 5) == 0)
+{
+if (i != 100)
 printf("Buzz ");
 else
-printf("%d ", i);
-putchar('\n');
+printf("Buzz");
+}
+else
+printf("%d", i);
+printf("\n");
 return (0);
 }
