6c99a1565f376cad25f5ad7553bf23cbaf6864ce
AmineNeifer <amineneifer2000@gmail.com>
2019-09-29 10:23:03 +0000

    fibonacci sequence is fun

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+int main(void)
+{
+long int i, j, s;
+int n;
+i = 0;
+j = 1;
+for (n = 0; n < 50; n++)
+if (n != 49)
+{
+s = i + j;
+printf("%ld, ", s);
+i = j;
+j = s;
+}
+ else
+{
+s = i + j;
+printf("%ld\n", s);
+}
+return (0);
+}
