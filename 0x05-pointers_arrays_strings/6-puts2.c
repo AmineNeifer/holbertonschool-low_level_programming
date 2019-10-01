f901973a235355c3292ff5b07afc5b769e62d078
anaruzz <laabidigh@gmail.com>
2019-10-01 08:25:09 +0000

    printing every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _puts2 - prints a string.
+ * @str: pointer
+ * Return: N/A .
+ */
+void puts2(char *str)
+{
+int n;
+n = 0;
+while (*(str + n) != '\0')
+{
+_putchar(*(str + n));
+n += 2;
+}
+_putchar('\n');
+}
