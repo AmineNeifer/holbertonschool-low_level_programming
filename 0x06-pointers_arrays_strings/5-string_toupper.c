e7ed61732f38ce57e3d135693982093c5eb5d764
anaruzz <laabidigh@gmail.com>
2019-10-03 12:04:47 +0000

    look up xd

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * *string_toupper - make all letters uppercase.
+ * @a: string.
+ * Return: all uppercase string.
+ */
+char *string_toupper(char *a)
+{
+int i = 0;
+int j;
+while (a[i] != '\0')
+i++;
+for (j = 0; j < i; j++)
+if (a[j] >= 'a' && a[j] <= 'z')
+a[j] -= 32;
+ return (a); 
+}
