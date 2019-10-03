f42cc4331976a24d32080d2eea29bcae1fe07200
anaruzz <laabidigh@gmail.com>
2019-10-03 12:39:23 +0000

    capitalizing words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * *cap_string - capitalising word.
+ * @c: string.
+ * Return: c.
+ */
+char *cap_string(char *c)
+{
+int i = 0;
+int j = 0;
+while (c[i] != '\0')
+i++;
+for(j = 0; j < i; j++)
+{
+if (c[j] >= 'a' && c[j] <= 'z')
+{
+if (c[j - 1] == ',' || c[j - 1] == ' ' || c[j - 1] == '.' || c[j - 1] == '!')
+c[j] -= 32;
+else if ( c[j - 1] == '?' || c[j - 1] == '"' || c[j - 1] == '(' || c[j - 1] == ')')
+c[j] -= 32;
+else if (c[j - 1] == '{' || c[j - 1] == '}' || c[j - 1] == '\n' || c[j - 1] == '\t')
+c[j] -= 32;
+}
+}
+return (c);
+}
