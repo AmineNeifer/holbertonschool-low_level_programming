90bc49a2c82b0bfb58a2ab8347c43b3b532d355b
anaruzz <laabidigh@gmail.com>
2019-10-08 11:22:42 +0000

    set a pointer to a pointer

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * set_string - setting a string to a pointer.
+ * @s: pointer to a pointer.
+ * @to: pointer.
+ * Return: N/A.
  */
 void set_string(char **s, char *to)
 {
