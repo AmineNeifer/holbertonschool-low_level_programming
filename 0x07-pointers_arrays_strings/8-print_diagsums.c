913d744c17fc8eb03cf088b98aadd06812b03449
anaruzz <laabidigh@gmail.com>
2019-10-08 11:25:53 +0000

    printing diagonal sums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * print_diagsums - printing sum.
- * @a: array;
- * @size: numbers of columns.
- * return: N/A.
+ * print_diagsums - print sum of diagonals of an array of array
+ * @a: array of int
+ * @size: size of array
+ * Return: void
  */
 void print_diagsums(int *a, int size)
 {
-int i;
-
- for (i = 0; i <= size; i++)
+int i, s1 = 0, s2 = 0;
+for (i = 0; i < size; i++)
 {
-printf("%d, ", *(a + i));
+s1 += *(a + i * size + i);
+s2 += *(a + (i * size) + (size - 1 - i));
 }
+printf("%d, %d\n", s1, s2);
 }
