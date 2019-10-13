d93ae549238b96f44c6745f0ae2c30cdb0491626
AmineNeifer <amineneifer2000@gmail.com>
2019-10-13 21:57:23 +0200

    argv and argc's

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+int coin_counter(int a);
 /**
  * main - print number of coins.
  * @argc: argument counter.
  * @argv: argument vector.
- * Return: 
+ * Return: 1 if there is an error otherwise 0.
  */
 int main(int argc, char *argv[])
 {
 int a, c;
-c = 0;
 if (argc != 2)
 {
 printf("Error\n");
@@ -21,34 +21,45 @@ if (a == 0)
 printf("0\n");
 return (0);
 }
-while (a > 0)
-{
-    if (a >= 25)
-    {
-    c = c + a / 25;
-    a %= 25;
-    }
-    else if (a >= 10)
-    {
-    c += a / 10;
-    a %= 10;
-    }
-    else if (a >= 5)
-    {
-    c += a / 5;
-    a %= 5;
-    }
-    else if (a >= 2)
-    {
-    c += a / 2;
-    a %= 2;
-    }
-    else if (a == 1)
-    {
-    c++;
-    a %= 1;
-    } 
-}
+c = coin_counter(a);
 printf("%d\n", c);
 return (0);
+}
+/**
+ * coin_counter - count the number of coins.
+ * @a: cents.
+ * Return: number of coins.
+ */
+int coin_counter(int a)
+{
+int c = 0;
+while (a > 0)
+{
+if (a >= 25)
+{
+c = c + a / 25;
+a %= 25;
+}
+else if (a >= 10)
+{
+c += a / 10;
+a %= 10;
+}
+else if (a >= 5)
+{
+c += a / 5;
+a %= 5;
+}
+else if (a >= 2)
+{
+c += a / 2;
+a %= 2;
+}
+else if (a == 1)
+{
+c++;
+a %= 1;
+}
+}
+return (c);
 }
