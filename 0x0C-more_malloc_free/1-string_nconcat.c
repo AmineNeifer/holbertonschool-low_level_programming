79440dc1df06c49ed6c4d2221b5f527537fe6ae0
AmineNeifer <amineneifer2000@gmail.com>
2019-10-17 23:14:32 +0200

    concatinating 2 str using malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,45 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "holberton.h"
 /**
-  * string_nconcat - concatinate two strings
-  * @s1: String
-  * @s2: String
-  * @n: unisgned integer
-  * Return: Null if fails, pointer to a string
+  * string_nconcat - concatinating two strings
+  * @s1: String.
+  * @s2: String type.
+  * @n: integer type.
+  * Return: p pointer.
   */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-
-	unsigned int i, len1 = 0, len2 = 0, final;
-	char *res;
-
-	if (s1 != NULL)
-	{
-	while (s1[len1])
-		len1++;
-	}
-	if (s2 != NULL)
-	{
-	while (s2[len2])
-		len2++;
-	}
-	if (n < len2)
-		len2 = n;
-
-	final = len1 + len2;
-
-		final++;
-		res = malloc(final * sizeof(char));
-		if (res == NULL)
-			return (NULL);
-		for (i = 0; i < len1; i++)
-			res[i] = s1[i];
-		for (i = 0; i < len2; i++)
-		{
-			res[i + len1] = s2[i];
-		}
-		res[final - 1] = '\0';
-
-return (res);
+char *p;
+unsigned int i, final;
+unsigned int i1 = 0;
+unsigned int i2 = 0;
+while (s1[i1] != '\0')
+i1++;
+while (s2[i2] != '\0')
+i2++;
+if (n < i2)
+i2 = n;
+final = i1 + i2;
+final++;
+p = malloc(final *sizeof(char));
+if (p == NULL)
+return (NULL);
+for (i = 0; i < i1; i++)
+p[i] = s1[i];
+for (i = 0; i < i2; i++)
+p[i + i1] = s2[i];
+p[final - 1] = '\0';
+return (p);
+}
