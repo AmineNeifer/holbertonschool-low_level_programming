4908dfc90c0bfd4a60ad4b5bbbf65291c8dc26b0
AmineNeifer <amineneifer2000@gmail.com>
2019-10-17 23:36:04 +0200

    similar to calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - similar to calloc function.
+ * @nmemb: number of elemets
+ * @size: number of bytes in one nmemb
+ * Return: pointer p.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int i;
+char *p = malloc (nmemb * size);
+if (p == NULL || nmemb == 0)
+return (0);
+for (i = 0; i < nmemb; i++)
+p[i] = 0;
+return (p);
+}
