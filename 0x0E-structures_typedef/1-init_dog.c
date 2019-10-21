edc985d75d95b04ddf4e1cc5c52acba7711644ed
AmineNeifer <amineneifer2000@gmail.com>
2019-10-21 22:46:00 +0200

    intializing struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * init_dog - intitialize a dog information.
+ * @name: its name.
+ * @age: its age.
+ * @owner: its owner.
+ * Return: void.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+dog->name = name;
+dog->age = age;
+dog->owner = owner;
+}
