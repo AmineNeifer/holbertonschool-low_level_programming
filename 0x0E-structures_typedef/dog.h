db2cb729341358d1aaa5b8e369f1480f03284dde
AmineNeifer <amineneifer2000@gmail.com>
2019-10-21 22:32:28 +0200

    testing header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+/**
+ * struct dog - important informations about the dog.
+ * @name: name of dog.
+ * @age: age of the dog.
+ * @owner: who is the owner of the dog.
+ */
 typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
-} dog ;
+char *name;
+float age;
+char *owner;
+} dog;
 #endif
