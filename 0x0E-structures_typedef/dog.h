3d1bdf2545e2161be176adfc5abf44282d116654
AmineNeifer <amineneifer2000@gmail.com>
2019-10-22 19:02:33 +0200

    dog header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,9 +13,5 @@ char *name;
 float age;
 char *owner;
 } dog;
-typedef struct dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-typedef struct dog dog_t;
-void free_dog(dog_t *d);
+
 #endif
