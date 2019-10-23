b30a4482d6791025134a4ba914d9803d3e88255d
AmineNeifer <amineneifer2000@gmail.com>
2019-10-23 20:02:26 +0200

    test

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t i;
-if (array != NULL || size != 0 || *action == 0)
+if (array != NULL || size != 0 || *action != 0)
+{
 for (i = 0; i < size; i++)
 (*action)(array[i]);
+}
 }
