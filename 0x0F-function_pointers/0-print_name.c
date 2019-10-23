43dddbab573d1cb78dc59c22caaf2e9251693abb
AmineNeifer <amineneifer2000@gmail.com>
2019-10-23 19:15:34 +0200

    function pointer how to

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,11 @@
 #include "function_pointers.h"
+/**
+ * print_name - takes a name and prints it.
+ * @name: string.
+ * @f: pointer to a function.
+ * Return: void.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+(*f)(name);
 }
