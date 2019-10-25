cefc017dbdfb216aa593f40aa129423e3036c3b2
AmineNeifer <amineneifer2000@gmail.com>
2019-10-25 16:21:03 +0200

    sum of args

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - calculating the sum of ints
+ * @n: a constant
+ * return: the sum of int arguments.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+if (n == 0) 
+return (0);
+va_list lis;
+int sum;
+unsigned int i;
+sum = 0;
+va_start(lis, n);
+for (i = 0; i < n; i++)
+sum += va_arg(lis, unsigned int);
+va_end(lis);
+return (sum);
+}
