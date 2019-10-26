66f5c713d479dbebb1e8526365fc2e37ef026d89
AmineNeifer <amineneifer2000@gmail.com>
2019-10-26 18:21:04 +0200

    headerr

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,12 @@
 #ifndef _VARIADIC_FUNCRIONS_H_
 #define _VARIADIC__FUNCTIONS_H_
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+typedef struct lol
+{
+char type;
+void (*func)();
+} lol;
 #endif
