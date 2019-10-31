af40c2172272a492041b340e6aee1ad10051300c
AmineNeifer <amineneifer2000@gmail.com>
2019-10-31 17:38:58 +0100

    first commit of the day

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - print all elements of a list.
+ * @h: constant list.
+ * Return: the number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+size_t l;
+l = 0;
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+l++;
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+l++;
+}
+
+if (h->next != NULL)
+return (l + print_list(h->next));
+return (l);
+}
