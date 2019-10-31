e87bdb1c8afdf712add90c5ce708852429bc0b7c
AmineNeifer <amineneifer2000@gmail.com>
2019-10-31 17:56:43 +0100

    c lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * list_len - calculates the number of lists it contains.
+ * @h: constant list.
+ * Return: the number of nodes.
+ */
+size_t list_len(const list_t *h)
+{
+size_t l;
+l = 0;
+l++;
+if (h->next != NULL)
+{
+return (l + list_len(h->next));
+}
+return (l);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
+#include <stdio.h>
 int _putchar(char c);
 /**
  * struct list_s - singly linked list
@@ -17,4 +18,5 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 #endif
