9a275993630e82634f0aeb1434d029fd664a4a31
Amine Neifer <amineneifer2000@gmail.com>
2019-11-05 15:55:03 +0100

    new commit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 #include <stdio.h>
+#include <stdlib.h>
 int _putchar(char c);
 /**
  * struct list_s - singly linked list
@@ -19,4 +20,6 @@ size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
 #endif
