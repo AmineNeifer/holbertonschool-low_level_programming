36e9b1ea86ceaa9f85b67a550433ae524e702a71
Amine Neifer <amineneifer2000@gmail.com>
2019-11-05 18:39:57 +0100

    deletere the first node in a list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+/**
+ * pop_listint - delete the first node of a list.
+ * @head: pointer to the first node.
+ * Return: n of the first node.
+ */
 int pop_listint(listint_t **head)
 {
 int d;
 listint_t *p;
 d = (*head)->n;
 p = (*head)->next;
-free (*head);
-*head = p; 
+free(*head);
+*head = p;
 return (d);
 }
