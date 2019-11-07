dd53e1839525fa38396fb12ffa6aded69b50c270
Amine Neifer <amineneifer2000@gmail.com>
2019-11-07 23:55:47 +0100

    getting bit by index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,23 +10,5 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 if (index > 31)
 return (-1);
-if (index == 0)
-{
-if (n & 1)
-return (1);
-else
-return (0);
-}
-else if (index == 1)
-{
-n = n >> 1;
-if (n & 1)
-return (1);
-else
-return (0);
-}
-n = n >> index;
-while (n != 0 && n != 1)
-n = n << 1;
-return (n);
+return ((n >> index) & 1);
 }
