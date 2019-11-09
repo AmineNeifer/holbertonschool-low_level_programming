62480031b5d0ec6f4e96cc398d03372284d358fd
Amine Neifer <amineneifer2000@gmail.com>
2019-11-09 13:39:28 +0100

    fixed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,14 +15,15 @@ int i1, i2, **p;
 if (width <= 0 || height <= 0)
 return (0);
 p = malloc(sizeof(int *) * height);
+if (p == NULL)
 return (0);
 for (i1 = 0; i1 < height; i1++)
 {
-p[i1] = malloc(sizeof(int) * width);
+p[i1] = malloc(sizeof(int *) * width);
 if (p[i1] == NULL)
 {
-for (i2 = 0; i2 < width; i2++)
-free(p[i1] + i2);
+for (i2 = 0; i2 < height; i2++)
+free(p[i2]);
 free(p);
 return (0);
 }
