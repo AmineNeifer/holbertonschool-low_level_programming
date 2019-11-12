d6bfc037fb2e7c8e845f9fca0b81d673d6185f71
Amine Neifer <amineneifer2000@gmail.com>
2019-11-12 19:31:27 +0100

    cpy

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,50 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - does the same as cp, the command
+ * @ac: arg counter.
+ * @av: arg vector.
+ * Return: 0.
+ */
+int main(int ac, char **av)
+{
+int fd, fd2, count = 1024, r, w, cl, cl1;
+char *buf = malloc(1024);
+if (ac != 3)
+{
+dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+fd2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0644);
+fd = open(av[1], O_EXCL | O_RDONLY);
+r = read(fd, buf, count);
+if (fd == -1 || r == -1)
+{
+dprintf(STDOUT_FILENO, "Error: Can't read to %s\n", av[1]);
+exit(98);
+}
+w = write(fd2, buf, r);
+if (fd2 == -1 || w == -1)
+{
+dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", av[2]);
+exit(99);
+}
+cl = close(fd);
+if (cl == -1)
+{
+dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd);
+exit(100);
+}
+cl1 = close(fd2);
+if (cl1 == -1)
+{
+dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd2);
+exit(100);
+}
+return (0);
+}
