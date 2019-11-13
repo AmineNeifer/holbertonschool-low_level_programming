aeada98c8bfbd13414841a2bba6b6254b9505080
Amine Neifer <amineneifer2000@gmail.com>
2019-11-13 18:25:11 +0100

    just trying

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,41 +14,38 @@
 int main(int ac, char **av)
 {
 int fd, fd2, count = 1024, r, w, cl, cl1;
-char *buf = malloc(1024);
+char *buf = malloc(5000);
+if (buf == NULL)
+return (0);
 if (ac != 3)
 {
-dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-fd2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0644);
+fd2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 fd = open(av[1], O_EXCL | O_RDONLY);
-if (fd == -1)
-{
-dprintf(STDOUT_FILENO, "Error: Can't read to %s\n", av[1]);
-exit(98);
-}
 r = read(fd, buf, count);
-if (r == -1)
+if (fd == -1 || r == -1)
 {
-dprintf(STDOUT_FILENO, "Error: Can't read to %s\n", av[1]);
+dprintf(STDERR_FILENO, "Error: Can't read to %s\n", av[1]);
 exit(98);
 }
 w = write(fd2, buf, r);
 if (fd2 == -1 || w == -1)
 {
-dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", av[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 exit(99);
 }
 cl = close(fd);
 if (cl == -1)
 {
-dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 exit(100);
 }
 cl1 = close(fd2);
 if (cl1 == -1)
 {
-dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd2);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 exit(100);
 }
 free(buf);
