5f0bd7fd19d807fc2d5261556e149727fbe019e3
Amine Neifer <amineneifer2000@gmail.com>
2019-11-27 20:00:28 +0100

    lol

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,23 +3,37 @@
 #include <stdio.h>
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    int *p;
-    unsigned int i;
-    char *old = ptr;
-    if (new_size == 0 && ptr != NULL)
-    {
-        free(ptr);
-        return (0);
-    }
-    p = malloc(new_size);
-    if (p == NULL)
-    return (0);
-    if (new_size == old_size)
-    return (ptr);
-    else if (new_size < old_size)
-    free (p);
-    for (i = 0; i < old_size; i++)
-    p[i] = old[i];
-    return (p);
+	char *new;
+	char *old;
 
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	new = malloc(new_size);
+	old = ptr;
+	if (new == NULL)
+		return (NULL);
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			new[i] = old[i];
+		free(ptr);
+	}
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			new[i] = old[i];
+		free(ptr);
+	}
+	return (new);
 }
