24437eb9dc1ecd2e13fd588c1254be71b21dbb50
amineneifer <amineneifer2000@gmail.com>
2019-12-13 18:40:01 +0000

    len of a list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * dlistint_len - returns number of elements.
+ * @h: head of the list.
+ * Return: number of elements in the list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+  size_t num = 0;
+  if (h == NULL)
+    return(0);
+  while (h)
+    {
+      num++;
+     h = h->next;
+    }
+  return (num);
+}
