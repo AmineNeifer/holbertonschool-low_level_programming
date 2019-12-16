0060f91009ef12baba65afa4729ec043f569f37f
amineneifer <amineneifer2000@gmail.com>
2019-12-16 09:06:48 +0000

    tawa

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -9,15 +9,16 @@
 int sum_dlistint(dlistint_t *head)
 {
 dlistint_t *current = head;
-int i = head->n;
+int i = 0;
 if (head == NULL)
 return (0);
 if (head && head->next == NULL)
 return (i);
 while (current && current->next)
 {
-current = current->next;
 i += current->n;
+current = current->next;
 }
+i += current->n;
 return (i);
 }
