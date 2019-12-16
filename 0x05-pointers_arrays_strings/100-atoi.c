70abf7c52f91d8094311e7bed8f6272d8e72fe02
amineneifer <amineneifer2000@gmail.com>
2019-12-16 13:00:06 +0100

    ya 7lili né

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,20 +1,28 @@
 #include "holberton.h"
 /**
  * atoi - transform char to int.
- * 
+ *
  * Return - integer.
  */
-int atoi(char *s)
+int _atoi(char *s)
 {
-int i = 0;
-int sign = 1;
-int j = 0;
-if (s[0] + '-')
-{
-sign = -1;
-i++;
-}
-for (; s[i] != '\0'; i++)
-j = j*10 + j[i] - '0';
-return (sign*j);
+	int found = 0;
+	unsigned int num = 0;
+	int n = 1;
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+			n *= -1;
+		else if (*s >= '0' && *s <= '9')
+		{
+			if (!found)
+				found = 1;
+			num = num * 10 + (*s - '0');
+		}
+		else if (found)
+			break;
+		s++;
+	}
+	return (num * n);
 }
