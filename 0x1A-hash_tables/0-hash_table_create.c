e27a8d235fa96b78466cfc78166a21ab59b7d996
amineneifer <amineneifer2000@gmail.com>
2020-01-18 16:39:13 +0000

    removed asterisk in mallocs and handled second malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,19 +8,19 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	unsigned long int i;
-	
-	table = malloc(sizeof(hash_table_t *));
+
+	if (size == 0)
+		return (NULL);
+
+	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
+
 	table->size = size;
-	table->array = malloc(size * sizeof(hash_node_t *));
-	for (i = 0; i < size; i++)
-	{
-		table->array[i] = NULL;
-	}
+	table->array = malloc(size * sizeof(hash_node_t));
+
+	if (!table->array)
+		return (NULL);
+
 	return (table);
-	
-
-
 }
