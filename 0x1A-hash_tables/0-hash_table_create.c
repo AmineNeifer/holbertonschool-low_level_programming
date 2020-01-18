55331e455c37cc05719008633ba0abd8d663cf4d
amineneifer <amineneifer2000@gmail.com>
2020-01-18 16:27:22 +0000

    remade the function from scratch

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,13 +9,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 	
-	if (size == 0)
-		return (NULL);
-
-	table = malloc(sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t *));
 	if (!table)
 		return (NULL);
 	table->size = size;
-	table->array = NULL;
+	table->array = malloc(size * sizeof(hash_node_t *));
+	
 	return (table);
+	
+
+
 }
