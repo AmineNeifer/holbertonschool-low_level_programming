7f8ec24b69b925cf5d7869a55fda3f473dae63bd
amineneifer <amineneifer2000@gmail.com>
2020-01-19 12:41:45 +0000

    cleared if key is an empty string return 0

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *next_node;
 
-	if (!key)
+	if (strcmp(key, "") == 0)
 		return (0);
 
 	next_node = malloc(sizeof(hash_node_t *));
