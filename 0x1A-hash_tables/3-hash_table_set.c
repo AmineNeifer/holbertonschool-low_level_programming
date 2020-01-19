5b22436d654a5551b87ce2aaf1f6a22ea991a8a7
amineneifer <amineneifer2000@gmail.com>
2020-01-19 11:34:05 +0000

    added the condition if there is more than one collision

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,26 +13,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *next_node;
+	hash_node_t *current;
 
 	if (!key)
 		return (0);
-
 	next_node = malloc(sizeof(hash_node_t *));
 	if (!next_node)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
+	current = ht->array[index];
 
 	next_node->key = strdup(key);
 	next_node->value = strdup(value);
 	next_node->next = NULL;
 
-	if (ht->array[index] != NULL)
-	{
-		ht->array[index]->next = next_node;
-	}
-	else
-	{
-		ht->array[index] = next_node;
-	}
+	while (current)
+		current = current->next;
+	current = next_node;
 	return (1);
 }
