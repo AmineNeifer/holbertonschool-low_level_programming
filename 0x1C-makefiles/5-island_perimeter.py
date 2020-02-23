f3dfcf3d3c373c0f24ddab489a414f725b97659c
amineneifer <amineneifer2000@gmail.com>
2020-02-23 12:44:06 +0000

    island perimeter, easy stuff

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""contains only one function : island_perimeter"""
+def island_perimeter(grid):
+    """ returns perimeter of the island described in grid
+    """
+    sum_of_1 = 0
+    for i in grid:
+        for j in i:
+            if j == 1:
+                sum_of_1 += 1
+    return (2 * sum_of_1 + 2)
