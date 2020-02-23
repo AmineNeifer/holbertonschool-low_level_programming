ddb89b43c6722cae5c143e7aef7186e818b1dc15
amineneifer <amineneifer2000@gmail.com>
2020-02-23 13:21:26 +0000

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,13 +1,45 @@
 #!/usr/bin/python3
 """contains only one function : island_perimeter"""
+
+
+def surrounding(grid, i, j):
+    """ calculates number of surroundes 1's"""
+    surr = 0
+
+    try:
+        if grid[i + 1][j] == 1:
+            surr += 1
+    except IndexError:
+        pass
+
+    try:
+        if grid[i - 1][j] == 1:
+            surr += 1
+    except IndexError:
+        pass
+
+    try:
+        if grid[i][j + 1] == 1:
+            surr += 1
+    except IndexError:
+        pass
+
+    try:
+        if grid[i][j - 1] == 1:
+            surr += 1
+    except IndexError:
+        pass
+    return surr
+
+
 def island_perimeter(grid):
     """ returns perimeter of the island described in grid
     """
-    sum_of_1 = 0
-    for i in grid:
-        for j in i:
-            if j == 1:
-                sum_of_1 += 1
-    if sum_of_1 == 0:
-        return 0
-    return (2 * sum_of_1 + 2)
+    peri = 0
+    i = -1
+    for line in grid:
+        i += 1
+        for j in range(len(line)):
+            if grid[i][j] == 1:
+                peri += 4 - surrounding(grid, i, j)
+    return peri
