5279a80f98d74988014ddb45d53f5224c03c20c9
amineneifer <amineneifer2000@gmail.com>
2020-04-15 20:30:21 +0000

    header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,4 +4,6 @@
 #include <stdio.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int rec(int *array, int deb, int end, int value);
+int jump_search(int *array, size_t size, int value);
 #endif /* _SEARCH_ALGOS_H_ */
